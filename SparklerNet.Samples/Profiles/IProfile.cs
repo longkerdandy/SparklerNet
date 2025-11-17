2008f2d45b1be316159a68a4ab23fe7140f9471a
Dennis Gu <longkerdandy@gmail.com>
2025-11-17 22:46:10 +0800

    Add profile support and scan rate commands to sample app

---
+++ SparklerNet.Samples/Profiles/IProfile.cs
@@ -0,0 +1,22 @@
+﻿using MQTTnet;
+using SparklerNet.Core.Options;
+
+namespace SparklerNet.Samples.Profiles;
+
+/// <summary>
+///     This interface defines the contract for a Sparkplug client profile.
+/// </summary>
+public interface IProfile
+{
+    /// <summary>
+    ///     Gets the MQTT client options for the profile.
+    /// </summary>
+    /// <returns>The MQTT client options.</returns>
+    public MqttClientOptions GetMqttClientOptions();
+    
+    /// <summary>
+    ///     Gets the Sparkplug client options for the profile.
+    /// </summary>
+    /// <returns>The Sparkplug client options.</returns>
+    public SparkplugClientOptions GetSparkplugClientOptions();
+}
